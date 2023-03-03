29610f535ea4cff890575ead56cfe1352b0af92d
jelimarl <jelimarlm@gmail.com>
2023-03-02 19:24:55 -0500

    Add swr to series file

--- pages/series.jsx
+++ pages/series.jsx
@@ -1,10 +1,33 @@
 import Header from "@/components/header";
+import { fetcher } from "@/utils/petition";
+import { useState } from "react";
+import useSWR from "swr";
 
 export default function Series() {
+  const [option, setOption] = useState('')
+
+  const urlAPI = "https://www.omdbapi.com/"
+  const apiKey = "dec040b6"
+
+  const { data, error, isLoading } = useSWR(`${urlAPI}?apikey=${apiKey}&type=series&s=${option}`, fetcher)
+
+  function handleChangeSelector(event) {
+    const seriesType = event.target.value
+    setOption(seriesType)
+  }
+
+  console.log(option)
+  console.log(data)
+
   return (
     <div>
       <Header></Header>
       <h1>Series</h1>
+      <select onChange={handleChangeSelector}>
+        <option>Choose</option>
+        <option value="love">Love</option>
+        <option value="christmas">Christmas</option>
+      </select>
     </div>
   )
 }
